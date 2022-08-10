d13757dd69bb5fb477707b7bab81f90b8069c64e
KevInZhao <hellozhaowenkai@gmail.com>
2022-08-10 21:09:46 +0800

    style(root): follow the official style guide

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <like-button
+    <LikeButton
       :initLikeStatus="initLikeStatus"
       @likeStatusChange="likeStatusChange"
     />
