73dccf3f731f50d5d72b7bb7a8b207f06e5e0a68
KevInZhao <hellozhaowenkai@gmail.com>
2022-08-12 17:03:47 +0800

    feat(root): add support for day.js

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,6 +11,7 @@
 <script>
 import LikeButton from "@/components/LikeButton";
 import { APILike } from "@/api/lab-django-demo";
+import dayjs from "dayjs";
 
 export default {
   name: "AboutView",
@@ -29,6 +30,8 @@ export default {
       const apiLike = new APILike();
       apiLike.pk = 1;
       apiLike.fields = { lastAddBy };
+
+      const startTime = dayjs();
       apiLike
         // .fetchList()
         // .fetchDetail()
@@ -37,15 +40,23 @@ export default {
         .updateOrCreate()
         // .drop()
         .then((data) => {
-          console.log("data", data);
+          console.log("$data", data);
         })
         .catch((error) => {
-          console.log("error", error);
+          console.log("$error", error);
+        })
+        .finally(() => {
+          const endTime = dayjs();
+          console.log(
+            "$serverTiming",
+            `${endTime.diff(startTime, "ms", true)}ms`
+          );
         });
     },
   },
 
   mounted() {
+    console.log("$currentTime", dayjs().format());
     console.log("$projectConfig", this.$projectConfig);
   },
 };
